e4a04e9491549cda8cd8503a900fde9f29a3a41c
Burak Şen <buraksen7@hotmail.com>
2024-08-14 09:58:28 +0200

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,11 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
+  base: "/sorting-visualiser/",
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },
-  base: "/sorting-visualiser/",
+  }
 })
